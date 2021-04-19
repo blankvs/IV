84049a75cfda100cce3122bb60a12f9afebd113e
Sergio Vega <hekinky@MacBook-Pro.local>
2021-04-18 23:35:45 -0600

    [IV]

--- Code.js
+++ Code.js
@@ -22,3 +22,38 @@ function getGrade (s1, s2, s3) {
     
     return res
   }
+
+  //Let's play! You have to return which player won! In case of a draw return Draw!.
+
+  const rps = (p1, p2) => {
+  
+    if ( p1 == p2 ) {
+      return "Draw!"
+    }
+    
+    if ( p1 == "rock" && p2 == "scissors" ) {
+      return "Player 1 won!"
+    }
+    if ( p1 == "scissors" && p2 == "paper" ) {
+      return "Player 1 won!"
+    }
+    if ( p1 == "paper" && p2 == "rock" ) {
+      return "Player 1 won!"
+    } else {
+      return "Player 2 won!"
+    }
+    
+  };
+
+  // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+  function grow(x){
+
+    let res = 1
+    
+    for ( let i = 0; i < x.length; i++) {
+      res *= x[i]
+    }
+    
+    return res 
+  }
